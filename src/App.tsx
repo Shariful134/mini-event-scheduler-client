f93951c5e53399343200c281363175ec36e83889
Shariful134 <shariful32213@gmail.com>
2025-07-21 00:29:14 +0600

    simple react and talwind setup done

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl text-green-300">
+        This is the Mini Event Scheduler
+      </h1>
+    </>
+  );
+}
+
+export default App;
